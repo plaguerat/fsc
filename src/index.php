c98e8b17c7b27cbc089ed861a221b692f2405090
Darren Green <darrensgreen@gmail.com>
2016-01-22 10:03:42 -0600

    Change colorize to imported function
    Change colorize to imported function

--- src/index.php
+++ src/index.php
@@ -1,9 +1,10 @@
 <?php
 require_once("phar://fsc.phar/help.php");
+require_once("phar://fsc.phar/helpers/colorize.php");
 // Simple function to colorize output easier
-$colorize = function ($string, $color) {
-    return sprintf("\033[%sm%s\033[0m", $color, $string);
-};
+// colorize = function ($string, $color) {
+//     return sprintf("\033[%sm%s\033[0m", $color, $string);
+// };
 
 $decorate = function ($string, $decor) {
     return sprintf("\e[%sm%s\e[0m", $decor, $string);
@@ -26,13 +27,13 @@ if (isset($opts["h"])) {
 
 // Without a store name, there is no need to proceed
 if (! isset($opts["storepath"])) {
-    fwrite(STDOUT, $colorize(strtoupper("no storepath entered, aborting!!! \r\n\r\n\r\n"), 31));
+    fwrite(STDOUT, colorize(strtoupper("no storepath entered, aborting!!! \r\n\r\n\r\n"), 31));
     exit(1);
 }
 
 // If the store isn't set, we can't really do anything
 if (! isset($opts["store"])) {
-    fwrite(STDOUT, $colorize(strtoupper("no store entered, aborting!!! \r\n\r\n\r\n"), 31));
+    fwrite(STDOUT, colorize(strtoupper("no store entered, aborting!!! \r\n\r\n\r\n"), 31));
     exit(1);
 }
 
@@ -43,7 +44,7 @@ $storePath = preg_replace("~/$~", "", $opts["storepath"]);
 // @TODO This might be overcome by checking if it is relative and prepending the
 // directory to it
 if (preg_match("~^\.~", $storePath)) {
-    fwrite(STDOUT, $colorize("Please use absolute paths\r\n\r\n", 34));
+    fwrite(STDOUT, colorize("Please use absolute paths\r\n\r\n", 34));
     exit;
 }
 $storeName = $opts["store"];
@@ -52,8 +53,8 @@ $storeName = $opts["store"];
 // This will change if relative paths are allowed
 
 // We have, at a minimun, the stores directory
-fwrite(STDOUT, $colorize("Time to create some new flux architecture\r\n\r\n", 4));
-fwrite(STDOUT, "Store name: " . $colorize($storeName, 34) . "\r\n");
+fwrite(STDOUT, colorize("Time to create some new flux architecture\r\n\r\n", 4));
+fwrite(STDOUT, "Store name: " . colorize($storeName, 34) . "\r\n");
 
 // Begin creating things :-)
 $lowerReplace = "[__type__]";
@@ -83,42 +84,42 @@ $files = [
 foreach ($files as $key => $value) {
     $dirPath = sprintf("%s/%s", $storePath, $key);
     if (! is_dir($dirPath)) {
-        fwrite(STDOUT, $colorize(sprintf("%s directory not found in %s\r\n\tAttempt to create it (Y/N) (N will skip):", $key, $storePath), 37));
+        fwrite(STDOUT, colorize(sprintf("%s directory not found in %s\r\n\tAttempt to create it (Y/N) (N will skip):", $key, $storePath), 37));
         $create = trim(fgets(STDIN));
         if (strcasecmp("Y", $create) === 0) {
-            fwrite(STDOUT, $colorize("Attempting to create directory\r\n", 32));
+            fwrite(STDOUT, colorize("Attempting to create directory\r\n", 32));
             if (! @mkdir($dirPath)) {
-                fwrite(STDOUT, $colorize("Could not create Directory, skipping\r\n", 37));
+                fwrite(STDOUT, colorize("Could not create Directory, skipping\r\n", 37));
                 sleep(2);
                 continue;
             }
-            fwrite(STDOUT, $colorize("Directory successfully created", 32));
+            fwrite(STDOUT, colorize("Directory successfully created", 32));
         } else {
-            fwrite(STDOUT, $colorize("Skipping!", 35));
+            fwrite(STDOUT, colorize("Skipping!", 35));
             continue;
         }
     }
     $filepath = sprintf("%s/%s/%s%s.jsx", $storePath, $key, $storeName, $value);
     fwrite(STDOUT, "\r\n\r\nChecking " . $filepath . "\r\n");
     if (file_exists($filepath)) {
-        fwrite(STDOUT, "\t" . $colorize($colorize(strtoupper("file exists, overwrite(Y/N): "), "1"), 34));
+        fwrite(STDOUT, "\t" . colorize(colorize(strtoupper("file exists, overwrite(Y/N): "), "1"), 34));
         $overwrite = trim(fgets(STDIN));
         if (strcasecmp($overwrite, "y") !== 0) {
-            fwrite(STDOUT, $colorize("Skipping " . $filepath . "\r\n", 32));
+            fwrite(STDOUT, colorize("Skipping " . $filepath . "\r\n", 32));
             continue;
         }
     }
-    fwrite(STDOUT, $colorize("\t ... Creating " . $filepath . "\r\n", 36));
+    fwrite(STDOUT, colorize("\t ... Creating " . $filepath . "\r\n", 36));
     $template = $parseString($key . ".txt");
     file_put_contents($filepath, $template);
 }
 
 fwrite(
     STDOUT,
-    $colorize(
-        $colorize(
+    colorize(
+        colorize(
             "\r\n\r\nAll your file are ready to "
-            . $colorize("rock and roll", 1) . " \r\n\r\n\r\n",
+            . colorize("rock and roll", 1) . " \r\n\r\n\r\n",
             37
         ),
         1
