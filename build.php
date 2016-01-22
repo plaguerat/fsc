e23adf1b4e033272fbf71ac47d1e69deaa64d57b
Darren Green <darrensgreen@gmail.com>
2016-01-22 08:50:50 -0600

    Main build script
    Main build script

---
+++ build.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php
+fwrite(STDOUT, "Building the phar archive\r\n");
+$srcRoot   = __DIR__ . "/src/";
+$buildRoot = __DIR__ . "/build/";
+
+$phar = new \Phar(
+    $buildRoot . "flux-store-create.phar",
+    \FilesystemIterator::CURRENT_AS_FILEINFO | \FilesystemIterator::KEY_AS_FILENAME,
+    "flux-store-create.phar"
+);
+$phar->setStub($phar->createDefaultStub("index.php"));
+$phar["index.php"]               = file_get_contents($srcRoot . "index.php");
+$phar["templates/actions.txt"]   = file_get_contents($srcRoot . "templates/actions.txt");
+$phar["templates/api.txt"]       = file_get_contents($srcRoot . "templates/api.txt");
+$phar["templates/constants.txt"] = file_get_contents($srcRoot . "templates/constants.txt");
+$phar["templates/stores.txt"]    = file_get_contents($srcRoot . "templates/stores.txt");
+$phar["help.php"]    = file_get_contents($srcRoot . "help.php");
+// $phar->buildFromDirectory($srcRoot . "templates");
+fwrite(STDOUT, "All done, enjoy");
