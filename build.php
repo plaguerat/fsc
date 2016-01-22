297acde2ba223060253f33721350d8641dfcc1c3
Darren Green <darrensgreen@gmail.com>
2016-01-22 10:05:07 -0600

    import in the PHAR
    import in the PHAR

--- build.php
+++ build.php
@@ -26,5 +26,6 @@ $phar["templates/api.txt"]       = file_get_contents($srcRoot . "templates/api.t
 $phar["templates/constants.txt"] = file_get_contents($srcRoot . "templates/constants.txt");
 $phar["templates/stores.txt"]    = file_get_contents($srcRoot . "templates/stores.txt");
 $phar["help.php"]                = file_get_contents($srcRoot . "help.php");
+$phar["helpers/colorize.php"]    = file_get_contents($srcRoot . "helpers/colorize.php");
 // $phar->buildFromDirectory($srcRoot . "templates");
 fwrite(STDOUT, "All done, enjoy");
